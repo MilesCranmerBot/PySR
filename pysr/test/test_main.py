ecd95a5b7de8d8acbaebc289f334639c1b186f45
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-08-08 01:00:35 +0000

    test: include TypeSpec tests in main suite
    Co-authored-by: Miles Cranmer <miles.cranmer@gmail.com>

--- pysr/test/test_main.py
+++ pysr/test/test_main.py
@@ -50,6 +50,7 @@ from .params import (
     DEFAULT_POPULATIONS,
     skip_if_beartype,
 )
+from .test_type_specs import TestTypeSpecs
 
 # Disables local saving:
 os.environ["SYMBOLIC_REGRESSION_IS_TESTING"] = os.environ.get(
@@ -2279,6 +2280,7 @@ def runtests(just_tests=False):
         TestLaTeXTable,
         TestDimensionalConstraints,
         TestGuesses,
+        TestTypeSpecs,
     ]
     if just_tests:
         return test_cases
